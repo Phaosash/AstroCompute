e11b54848a9dfda54b0ba3487f097c52ce3ada7c
Phaosash <michael_mckie@hotmail.com>
2025-08-29 11:24:20 +0800

    Added error handling class and started implementing the server logic

---
+++ ErrorLogging/LoggingManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ErrorLogging;
+
+public sealed class LoggingManager {
+    private static readonly Lazy<LoggingManager> _instance = new(() => new LoggingManager(new FileLogger()));
+    private readonly FileLogger _fileLogger;
+
+    private LoggingManager (FileLogger fileLogger){
+        _fileLogger = fileLogger ?? throw new ArgumentNullException(nameof(fileLogger));
+    }
+
+    public static LoggingManager Instance => _instance.Value;
+
+    public void LogInformation (string message){
+        _fileLogger.Log(LogLevel.Information, message, null, (state, exception) => $"Message: {state}");
+    }
+
+    public void LogError (Exception ex, string message){
+        _fileLogger.Log(LogLevel.Error, message, ex, (state, exception) => $"{state}: {exception?.Message}\nStackTrace: {exception?.StackTrace}");
+    }
+
+    public void LogWarning (string message){
+        _fileLogger.Log(LogLevel.Warning, message, null, (state, exception) => $"Warning: {state}");
+    }
+
+    public void LogWarningWithException (Exception ex, string message){
+        _fileLogger.Log(LogLevel.Warning, message, ex, (state, exception) => $"Warning: {state}. Exception: {exception?.Message}");
+    }
+}
---
+++ WebServer/Services/IAstroContract.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServer.Services;
+
+public interface IAstroContract {
+    double EventHorizon (double a);
+    double Kelvin (double a);
+    double StarDistance (double a);
+    double StarVelocity (double a, double B);
+}
